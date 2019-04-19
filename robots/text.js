e47cb02eb8c306f99fa39d4d7eab60914953bd16
Daniel Rigoni <danielrgn@hotmail.com>
2019-04-19 11:43:05 -0300

    feat: start text robot

---
+++ robots/text.js
@@ -0,0 +1,61 @@
+const algorithmia = require("algorithmia");
+const algorithmiaApiKey = require("../credentials/algorithmia.json");
+const sbd = require('sbd')
+
+async function robot(content) {
+    await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    breakContentIntoSentences(content)
+
+    async function fetchContentFromWikipedia(content) {
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey.apiKey);
+        const wikipediaAlgoritm = algorithmiaAuthenticated.algo(
+          "web/WikipediaParser/0.1.2?timeout=300"
+        );
+        const wikipediaResponse = await wikipediaAlgoritm.pipe(
+          content.searchTerm
+        );
+        const wikipediaContent = wikipediaResponse.get()
+        
+        content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(
+          content.sourceContentOriginal
+        );
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+        content.sourceContentSanitezed = withoutDatesInParentheses
+        
+        function removeBlankLinesAndMarkdown(text) {
+          const allLines = text.split("\n");
+
+          const withoutBlankLinesAndMarkdown = allLines.filter(line => {
+            if (line.trim().length === 0 || line.trim().startsWith('=')) {
+              return false;
+            }
+            return true;
+          });
+
+          return withoutBlankLinesAndMarkdown.join(' ');
+        }
+        function removeDatesInParentheses(text) {
+          return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, "").replace("/  /g", " ");
+        }
+    }
+
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+
+        const sentences = sbd.sentences(content.sourceContentSanitezed)
+        sentences.forEach(sentence => {
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
+            })
+        })
+    }
+}
+
+module.exports = robot;
